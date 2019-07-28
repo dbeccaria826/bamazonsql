3a4ee845b31b1d6fc8f6c460964ddb7e730b759c
Dan Beccaria <beccdaniel@gmail.com>
2019-07-28 17:08:39 -0400

    added user interaction to check if they'd like to buy something

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql")
 const root_password = require("./password")
+const inquirer = require("inquirer")
 // setting up mysql connection
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -9,12 +10,38 @@ const connection = mysql.createConnection({
     database: 'bamazon'
 })
 
-connection.connect((error) => {
-    if(error) {
-        console.log(error)
+
+
+//User wants to make a selection if they decide to buy anything
+inquirer.prompt([
+    {
+        type:"list",
+        name:"sale",
+        message:"Interested in buying anything?",
+        choices: ["Yes", "I said Good Day!"]
+
     }
-    console.log("Connection Success")
+]).then(response => {
+    response.sale === "Yes" ? fundMyLifeStyle() : console.log("Jeff Bezos is creepy")
+    
 })
 
 
+let fundMyLifeStyle = () => {
 
+    connection.connect((error) => {
+        if(error) throw error
+        // console.log("Connection Success")
+    })
+
+    connection.query("SELECT * FROM inventory", (error, results) => {
+        if(error) throw error
+        // console.log(results)
+    
+        for(let item in results) {
+            // console.log(results[item])
+            // console.log(`${results[item].item_id} ${results[item].item_name} ${results[item].category_name}`)
+            console.log(`Item Name: ${results[item].item_name} Price: ${results[item].price}`)
+        }
+    })
+}
