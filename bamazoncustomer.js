3fdcaa83a1b64799d4fb8da997813dd0e110e62e
Dan Beccaria <beccdaniel@gmail.com>
2019-07-28 18:30:16 -0400

    added a table to display info from the database

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,7 +1,10 @@
 const mysql = require("mysql")
 const root_password = require("./password")
+const table = require("cli-table")
 const inquirer = require("inquirer")
 // setting up mysql connection
+
+
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -13,21 +16,27 @@ const connection = mysql.createConnection({
 
 
 //User wants to make a selection if they decide to buy anything
-inquirer.prompt([
-    {
-        type:"list",
-        name:"sale",
-        message:"Interested in buying anything?",
-        choices: ["Yes", "I said Good Day!"]
-
-    }
-]).then(response => {
-    response.sale === "Yes" ? fundMyLifeStyle() : console.log("Jeff Bezos is creepy")
+//Return a list of items person can choose from and place into their "cart"
+//Get the total price of all the items when person is finished shopping
+//Update database with current number of items purchased in stock after purchase
+let getCustomer = () => {
+    inquirer.prompt([
+        {
+            type:"list",
+            name:"sale",
+            message:"Interested in buying anything?",
+            choices: ["Yes", "I said Good Day!"]
     
-})
+        }
+    ]).then(response => {
+        response.sale === "Yes" ? inventory() : console.log("Come back soon")
+        
+    })
+}
 
 
-let fundMyLifeStyle = () => {
+
+let inventory = () => {
 
     connection.connect((error) => {
         if(error) throw error
@@ -35,13 +44,23 @@ let fundMyLifeStyle = () => {
     })
 
     connection.query("SELECT * FROM inventory", (error, results) => {
+        const productTable = new table( {
+            head: ["ITEM_ID", "ITEM_NAME","PRICE"],
+            colWidths: [20, 50, 30]
+        })
+
         if(error) throw error
         // console.log(results)
     
+        
         for(let item in results) {
-            // console.log(results[item])
+            let info = results[item]
+            
             // console.log(`${results[item].item_id} ${results[item].item_name} ${results[item].category_name}`)
-            console.log(`Item Name: ${results[item].item_name} Price: ${results[item].price}`)
+            productTable.push([info.item_id, info.item_name, `$${info.price}`])
         }
+        console.log(productTable.toString())
     })
 }
+
+getCustomer()
