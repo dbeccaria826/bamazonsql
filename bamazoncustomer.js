4889ef6baea680824c0ff14fd57592927b802ff5
Dan Beccaria <beccdaniel@gmail.com>
2019-07-30 16:18:57 -0400

    refactored functions that pull data from table

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -13,12 +13,21 @@ const connection = mysql.createConnection({
     password: root_password.id,
     database: 'bamazon'
 })
-const productTable = new table( {
-    head: ["ITEM_ID", "ITEM_NAME","CATEGORY", "PRICE"],
+
+
+const productTable = new table({
+    head: ["ITEM_ID", "ITEM_NAME", "CATEGORY", "PRICE"],
     colWidths: [20, 50, 30, 30]
 })
-let pushTable = (productTable,a,b,c,d) => {
-     return productTable.push([a,b,c,d])
+let pushTable = (productTable, results) => {
+    for (let item in results) {
+        let info = results[item]
+        productTable.push([info.item_id, info.item_name, info.category_name, `$${info.price}`])
+
+    }
+
+
+
 }
 
 
@@ -30,44 +39,39 @@ let pushTable = (productTable,a,b,c,d) => {
 let getCustomer = () => {
     inquirer.prompt([
         {
-            type:"list",
-            name:"sale",
-            message:"Interested in buying anything?",
+            type: "list",
+            name: "sale",
+            message: "Interested in buying anything?",
             choices: ["Yes", "I said Good Day!"]
-    
+
         }
     ]).then(response => {
-        response.sale === "Yes" ? sortItems() : console.log("Come back soon")
+        response.sale === 'Yes' ? sortItems() : console.log('See you around!')
+     
 
 
-})
+    })
 }
 
 //Shows the whole table (without filtered results)
 let inventory = () => {
 
-    connection.connect((error) => {
-        if(error) throw error
-        // console.log("Connection Success")
-    })
+
 
     connection.query("SELECT * FROM inventory", (error, results) => {
-       
 
-        if(error) throw error
+
+        if (error) throw error
         // console.log(results)
-    
-        
-        for(let item in results) {
-            let info = results[item]
-            pushTable(productTable,info.item_id, info.item_name, info.category_name, `$${info.price}`)
-        }
+
+
+        pushTable(productTable, results)
         console.log(productTable.toString())
     })
     setTimeout(() => {
-       makePurchase() 
+        makePurchase()
     }, 500);
-   
+
 }
 
 
@@ -75,15 +79,16 @@ let inventory = () => {
 let sortItems = () => {
     inquirer.prompt([
         {
-            type:"list",
-            name:"categories",
-            message:"Anything in particular?",
-            choices:['Hardware','Electronics','Fashion','Literature','Sports','All']
+            type: "list",
+            name: "categories",
+            message: "Anything in particular?",
+            choices: ['Hardware', 'Electronics', 'Fashion', 'Literature', 'Sports', 'All']
         }
     ]).then(response => {
-        switch(response.categories) {
+        switch (response.categories) {
             case "Hardware":
                 whichItems('Hardware')
+                
                 break;
             case "Electronics":
                 whichItems('Electronics')
@@ -97,60 +102,53 @@ let sortItems = () => {
             case "Sports":
                 whichItems('Sports')
                 break;
-            case "Scholarly":
-                whichItems('Scholarly')
-                break;
             default:
-              inventory()
+                return inventory()
         }
     })
-  
+
 }
 //Sorts items from db based on choice made
 let whichItems = (catName) => {
-    connection.connect((error) => {
-        if(error) throw error
 
-    })
 
     connection.query(`SELECT * FROM inventory WHERE category_name='${catName}'`, (error, results) => {
-        if(error) throw error
-        
-      
-            getItems(catName, results)
-        
-        console.log(productTable.toString())
+        if (error) throw error
+        pushTable(productTable, results)
+        getItems(catName)
+
+
     })
+
     setTimeout(() => {
         makePurchase()
-    }, 500); 
+    }, 500);
 }
 
-let getItems = (catName, results) => {
-    for(let item in results) {
-        let info = results[item]
-        switch(catName) {
-            case "Hardware":
-                pushTable(productTable,info.item_id, info.item_name, info.category_name, `$${info.price}`)
-               
-                break;
-            case "Fashion":
-                pushTable(productTable,info.item_id, info.item_name, info.category_name, `$${info.price}`)
-                break;
-            case "Electronics":
-                pushTable(productTable,info.item_id, info.item_name, info.category_name, `$${info.price}`)
-                break;
-            case "Literature":
-                pushTable(productTable,info.item_id, info.item_name, info.category_name, `$${info.price}`)
-                break;
-            case "Sports":
-                pushTable(productTable,info.item_id, info.item_name, info.category_name, `$${info.price}`)
-                break;
-            default:
-               inventory()
-            
-        }
-}
+let getItems = (catName) => {
+
+    switch (catName) {
+        case "Hardware":
+            return console.log(productTable.toString())
+
+
+        case "Fashion":
+            return console.log(productTable.toString())
+
+        case "Electronics":
+            return console.log(productTable.toString())
+
+        case "Literature":
+            return console.log(productTable.toString())
+
+        case "Sports":
+            return console.log(productTable.toString())
+
+        default:
+
+
+    }
+
 }
 
 // getCustomer()
@@ -159,66 +157,66 @@ let getItems = (catName, results) => {
 // If an item is out of stock it can be deleted from the base and the person can be notified.
 // JOIN INNER UPDATE
 let makePurchase = () => {
-    
+
     // console.log('Select Item by ID')
     inquirer.prompt([
         {
-            type:'input',
-            name:'select',
-            message:"Select Item by ID",
-            validate: function(name) {
+            type: 'input',
+            name: 'select',
+            message: "Select Item by ID",
+            validate: function (name) {
                 return /^[0-9]*$/.test(name)
             }
-            
+
         },
         {
-            type:'input',
-            name:'howmany',
-            message:'How many of this item?',
-            validate: function(name) {
+            type: 'input',
+            name: 'howmany',
+            message: 'How many of this item?',
+            validate: function (name) {
                 return /^[0-9]*$/.test(name)
             }
         }
     ]).then(response => {
-        
+
         connection.query(`SELECT * FROM inventory WHERE item_id='${response.select}'`, (error, result) => {
-            if(error) throw error
-            
+            if (error) throw error
+
             //What happens if a customer puts in 0 for howmany? 
+            //Update db to reflect total number of units sold per customer transaction
             //Controlling customer response
-            for(let item in result) {
+            for (let item in result) {
                 let info = result[item];
-                
-                if(response.howmany > info.quantity) {
+
+                if (response.howmany > info.quantity) {
                     console.log("Out of Stock!")
                 } else if (response.howmany > 0) {
-                   let customerTotal = response.howmany * info.price
+                    let customerTotal = response.howmany * info.price
                     console.log(`Your new total is ${customerTotal}`)
                 }
             }
             //We get the response, which one and how many
-            
-           
+
+
         })
-            
-       
-        connection.query("UPDATE inventory SET quantity = quantity - ? WHERE item_id = ?", [response.howmany, response.select] , (error,show) => {
-            if(error) throw error
+
+
+        connection.query("UPDATE inventory SET quantity = quantity - ? WHERE item_id = ?", [response.howmany, response.select], (error, show) => {
+            if (error) throw error
 
             // console.log(show)
-            
-        }) 
-        connection.query("UPDATE inventory SET sales = ? WHERE item_id = ?", [response.howmany, response.select], (error, show) => {
-            if(error) throw error
-            console.log(show)
+
         })
+        connection.query("UPDATE inventory SET sales = ? WHERE item_id = ?", [response.howmany, response.select], (error, show) => {
+            if (error) throw error
+
+
+        })
+        
         connection.end()
-        setTimeout(() => {
-            getCustomer()
-        }, 1000);
-       
+
     })
-    
+
 }
 //Need to display the person's total after they've selected how many of what item to purchase
 
